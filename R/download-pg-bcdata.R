52dbdb4c5859786c85ad740014ce26ce4d7daf85
NewGraphEnvironment <al@newgraphenvironment.com>
2020-10-05 14:03:01 -0700

    check for date stamp and only download if not present or not the same as the data bc record

--- R/download-pg-bcdata.R
+++ R/download-pg-bcdata.R
@@ -15,132 +15,103 @@
   options(timeout=180)##increase your timeout limit to allow download of bigger files
 }
 
-bcdc_get_record("7ecfafa6-5e18-48cd-8d9b-eae5b5ea2881")
-bcdc_tidy_resources("7ecfafa6-5e18-48cd-8d9b-eae5b5ea2881")
-
-
-##here we type our search term in to look for layers. Could use bcdc_browse() 
-bcdc_search("pscis-assessments", type = "Geographic", n = 83)
-bcdc_get_record("7ecfafa6-5e18-48cd-8d9b-eae5b5ea2881")
-
-#ecosections-ecoregion-ecosystem-classification-of-british-columbia : ccc01f43-860d-4583-8ba4-e72d8379441e
-#utm-zones-of-british-columbia
-#water-rights-licences-public :5549cae0-c2b1-4b96-9777-529d9720803c
-#pscis-assessments : 7ecfafa6-5e18-48cd-8d9b-eae5b5ea2881
-
-##should start a lookup table for these layers
-get_this <- bcdc_tidy_resources("pscis-assessments") %>% 
-  filter(bcdata_available == T)  %>% 
-  pull(package_id)
+# bcdc_get_record("7ecfafa6-5e18-48cd-8d9b-eae5b5ea2881")
+# bcdc_tidy_resources("7ecfafa6-5e18-48cd-8d9b-eae5b5ea2881")
+# 
+# 
+# ##here we type our search term in to look for layers. Could use bcdc_browse() 
+bcdc_search("pscis", type = "Geographic", n = 83)
+bcdc_get_record("572595ab-0a25-452a-a857-1b6bb9c30495")
+# 
+# #ecosections-ecoregion-ecosystem-classification-of-british-columbia : ccc01f43-860d-4583-8ba4-e72d8379441e
+# #utm-zones-of-british-columbia
+# #water-rights-licences-public :5549cae0-c2b1-4b96-9777-529d9720803c
+# #pscis-assessments : 7ecfafa6-5e18-48cd-8d9b-eae5b5ea2881
+# 
+# ##should start a lookup table for these layers
+# get_this <- bcdc_tidy_resources("pscis-assessments") %>% 
+#   filter(bcdata_available == T)  %>% 
+#   pull(package_id)
   
 ##name the layer you want to download
-get_this <- "7ecfafa6-5e18-48cd-8d9b-eae5b5ea2881"
+get_this <- "1596afbf-f427-4f26-9bca-d78bceddf485"
 
+get <- get_this
 
-bcdatapg <- function(get_this, crs = 3005)
-{
-    # drv <- dbDriver("PostgreSQL")
-    conn <- dbConnect(dbDriver("PostgreSQL"), 
-                      dbname = "postgis",
-                      host = "localhost", 
-                      port = "5432",
-                      user = "postgres", 
-                      password = "postgres")
-  date_stamp <- bcdc_get_record(get_this)[["metadata_modified"]]
-  # check_date_stamp <- dbGetQuery(conn, paste0("select d"))  ##in progress - we need to see if there is a "last_updated" column 
-  ## and if there is we compare to the date_stamp. If they are the same don't download - throw out the info or just move on.
-  dl <- bcdc_get_data(get_this)
-  layer_name <- dl$id[1]
-  schema_name <- tolower(word(layer_name,1,sep = "\\."))
-  table_name <- tolower(word(layer_name,2,sep = "\\."))
+##define what happens when we go to download
+bcdata_download <- function(get = get_this, crs = 3005){
+  conn <- dbConnect(dbDriver("PostgreSQL"), 
+                    dbname = "postgis",
+                    host = "localhost", 
+                    port = "5432",
+                    user = "postgres", 
+                    password = "postgres") 
+  dl <- bcdc_get_data(get)
+  date_stamp <- bcdc_get_record(get)[["metadata_modified"]]
+  object_name <- bcdc_get_record(get)[["object_name"]]
+  schema_name <- tolower(word(object_name,1,sep = "\\."))
+  table_name <- tolower(word(object_name,2,sep = "\\."))
   names(dl) <- tolower(names(dl))
   dl <- dl %>% 
     rename(geom = geometry) %>% 
     mutate(last_updated = date_stamp)
   classes <-  c("sf", "tbl_df", "tbl", "data.frame")
   class(dl) <- classes
-  dbExecute(conn, paste0("CREATE SCHEMA IF NOT EXISTS ", schema_name,";"))
+  res <- dbSendQuery(conn, paste0("CREATE SCHEMA IF NOT EXISTS ", schema_name,";"))
+  dbClearResult(res)
   dbWriteTable(conn, c(schema_name, table_name), value = dl, overwrite = TRUE)
-  dbExecute(conn, paste0("ALTER TABLE ", schema_name, ".", table_name, " ALTER COLUMN geom TYPE geometry;"))
-  dbExecute(conn, paste0("ALTER TABLE ", schema_name, ".", table_name, " ADD PRIMARY KEY (",'"objectid"',");")) ##assign primary key
+  res <- dbSendQuery(conn, paste0("ALTER TABLE ", schema_name, ".", table_name, " ALTER COLUMN geom TYPE geometry;"))
+  dbClearResult(res)
+  res <- dbSendQuery(conn, paste0("ALTER TABLE ", schema_name, ".", table_name, " ADD PRIMARY KEY (",'"objectid"',");")) ##assign primary key
+  dbClearResult(res)
   #create a spatial index 
-  dbExecute(conn, paste0("CREATE INDEX ON ", schema_name, ".", table_name, " USING GIST (geom)"))
-  dbExecute(conn,
-                    paste0("ALTER TABLE ", schema_name, ".", table_name, " ALTER COLUMN geom
+  res <- dbSendQuery(conn, paste0("CREATE INDEX ON ", schema_name, ".", table_name, " USING GIST (geom)"))
+  dbClearResult(res)
+  res <- dbSendQuery(conn,
+            paste0("ALTER TABLE ", schema_name, ".", table_name, " ALTER COLUMN geom
            Type geometry(Point, ", crs, ")
            USING ST_SetSRID(geom, ", crs, ");"))
+  dbClearResult(res)
   dbDisconnect(conn)
-  # rm(conn, drv, dl, classes, dsn_database, dsn_hostname, dsn_port, dsn_pwd,dsn_uid,
-  #    layer_name) 
+}
+
+##define a function that tests for date stamp before download
+bcdatapg <- function(get = get_this)
+{
+  # drv <- dbDriver("PostgreSQL")
+  conn <- dbConnect(dbDriver("PostgreSQL"), 
+                    dbname = "postgis",
+                    host = "localhost", 
+                    port = "5432",
+                    user = "postgres", 
+                    password = "postgres")
+  date_stamp <- bcdc_get_record(get)[["metadata_modified"]]
+  object_name <- bcdc_get_record(get)[["object_name"]]
+  schema_name <- tolower(word(object_name,1,sep = "\\."))
+  table_name <- tolower(word(object_name,2,sep = "\\."))
+  # check_date_stamp <- dbGetQuery(conn, paste0("select d"))  ##in progress - we need to see if there is a "last_updated" column 
+  ## and if there is we compare to the date_stamp. If they are the same don't download - throw out the info or just move on.
+  pg_table_colunms <- dbGetQuery(conn, paste0("SELECT column_name 
+                                              FROM information_schema.columns 
+                                              WHERE table_schema = '", schema_name,
+                                              "' AND table_name = '", table_name, "';")) %>% 
+    dplyr::pull()
+  
+  if('last_updated' %in% pg_table_colunms) {
+    if(dbGetQuery(conn, paste0("SELECT DISTINCT x.last_updated FROM ", schema_name,
+                               ".", table_name, " x;")) != date_stamp){
+      dbDisconnect(conn)
+      bcdata_download()
+    } else {
+      print('this table is up to date already')
+      }
+    } else {
+      bcdata_download()
+  }
+# rm(conn, drv, dl, classes, dsn_database, dsn_hostname, dsn_port, dsn_pwd,dsn_uid,
+#    layer_name) 
 } 
 
-
-bcdatapg(get_this = get_this)
-
-
-##if you want to set the crs I think you can do this. Might be glitchy though.
-dbGetQuery(conn, 
-           "ALTER TABLE whse_fish.pscis_assessment_svw
-           ALTER COLUMN geom
-           Type geometry(Point, 3005)
-           USING ST_SetSRID(geom, 3005);")
-
-##check to see that you have a unique identifier
-# names(dl)
-# n_distinct(dl$objectid) == nrow(dl)
-# n_distinct(dl$objectid)
-# nrow(dl)
-
-# ##look at your duplicates
-# duplicates <- dl %>%
-#   group_by(objectid) %>%
-#   filter(n() > 1) 
-# 
-# ##remove duplicates in a timely fashion
-# dl <- dl %>%
-#   group_by(objectid) %>%
-#   slice(1) 
-# 
-# ##remove duplicated rows (way slow)
-# dl <- dl %>%
-#   distinct(id, .keep_all = TRUE)
-
-##add a column for the FID to use as the primary key if you don't have a unique ID (i.e. objectid)
-# dl <- mutate(dl, fid = as.integer(rownames(dl))) %>%
-#   select(fid, everything())
-# names(dl)
+bcdatapg()
 
 
-
-
-##_________looking for layers and getting info about them____________________________________
-library(jsonlite)
-
-search_term <- "env-regional-boundaries"
-
-bcdc_api_url = "https://catalogue.data.gov.bc.ca/api/3/action/"
-wfs_url = "https://openmaps.gov.bc.ca/geo/pub/wfs"
-
-##build the search url for the api
-search_url <- paste0(bcdc_api_url, "package_search?q=", search_term)
-
-##this is requesting the info about the files 
-search_return <- fromJSON(readLines(search_url), flatten = TRUE)
-
-
-##let's isolate the layer we are looking for 
-search_return <- search_return$result 
-search_return <- search_return$results
-#matches$layer_name
-names(search_return)
-search_return$name
-target_layer_info <- filter(search_return, name == get_this) ##this refers to the search term at the top - might fuck you up a bit....
-target_layer_info$object_name
-#layer$id
-
-##see when the data was modified last
-target_layer_info$record_last_modified
-
-##here we can preview on imap
-bcdc_get_record("pscis-habitat-confirmations") [["preview_map_service_url"]] ##get resource id
-test <- bcdc_query_geodata("1915c8e2-3c8f-494f-bfc8-ecc013d494c5") %>%  collect()
